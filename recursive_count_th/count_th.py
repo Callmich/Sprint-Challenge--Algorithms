39efd48389603ce9733edaa688ada78d34809f88
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-24 09:53:01 -0600

    first pass is failing tests. Digging into why

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,11 +4,15 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
+    # Do I need a count? (revise)
+
     # Need to set a base case - in this case I think it would be best to be moving the string closer to 1 character as it cannot have a th if there is only one character
-
+    if len(word) > 2: # need to account for an empty string
+        return 0
     # Next I think I want to return the recursion. in this case I think it would be best to run the same function but just eliminating the first character of the string of the string
-
+    return count_th(word[1:])
     # if the first two characters of the string are th we can return 1
     
+    if word.startswith('th'):
+        return 1
     
-    pass
