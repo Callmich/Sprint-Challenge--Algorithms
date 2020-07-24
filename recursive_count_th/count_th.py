1bb710299a01b40ce85b860779637360ae522955
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-24 10:36:39 -0600

    forgot to add a differnt case after reviewing material from class. We are now accounting for two differnt cases rather than the one I was pursuing. This is now passing

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,42 +3,20 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-# def count_th(word):
-#     # Do I need a count? (revise)
-#     count = 0
-#     # Need to set a base case - in this case I think it would be best to be moving the string closer to 1 character as it cannot have a th if there is only one character
-
-#     def helper(x):
-#         nonlocal count
-
-#         if len(x) < 2: # need to account for an empty string
-#             return 0
-
-#     # if the first two characters of the string are th we can return 1
-#         if x.startswith('th'):
-#             count =+ 1
-
-#     # Next I think I want to return the recursion. in this case I think it would be best to run the same function but just eliminating the first character of the string of the string
-#         return count_th(x[1:])
-    
-#     helper(word)
-
-#     return count
-
-
 def count_th(word):
+    # Do I need a count? (revise)
     count = 0
-    if len(word) < 2: # need to account for an empty string
-        return count + 0
-    def helper(x):
-        
-        if x.startswith('th'):
-            return count + 1
-            
+    # Need to set a base case - in this case I think it would be best to be moving the string closer to 1 character as it cannot have a th if there is only one character
+    if len(word)< 2: # need to account for an empty string
+        return 0
+    # if word begins with th we need to add one and recurse
+    elif word.startswith("th"):
+        return count_th(word[1:]) + 1
+    # otherwise chop one off and run it again
+    else:
+        return count_th(word[1:])
+
     
-    count_th(word[1:])
-    helper(word)
-    return count
 
         
 
