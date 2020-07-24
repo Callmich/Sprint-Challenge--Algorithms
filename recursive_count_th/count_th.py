d4be964720d707b6dad35e18fb3ea56c99beb326
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-24 09:42:57 -0600

    wrote some pseudo code for the count problem. I need to research a bit more on eliminating the first character of a string

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,11 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
+    # Need to set a base case - in this case I think it would be best to be moving the string closer to 1 character as it cannot have a th if there is only one character
+
+    # Next I think I want to return the recursion. in this case I think it would be best to run the same function but just eliminating the first character of the string of the string
+
+    # if the first two characters of the string are th we can return 1
     
-    # TBC
     
     pass
